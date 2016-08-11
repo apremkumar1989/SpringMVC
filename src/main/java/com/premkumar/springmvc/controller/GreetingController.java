e4837836fa1b493e9c53838839f3d27ef9fc9c60
Prem Kumar <premkumar@Prems-MacBook-Pro-2.local>
2016-08-11 20:15:00 +0530

    default param value

--- src/main/java/com/premkumar/springmvc/controller/GreetingController.java
+++ src/main/java/com/premkumar/springmvc/controller/GreetingController.java
@@ -5,18 +5,25 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
 
 	@PostConstruct
-	public void init(){
+	public void init() {
 		System.out.println("-------------------------------------------------------------------------");
 	}
-	
-	@RequestMapping(value="/greeting",method={RequestMethod.GET})
-	public @ResponseBody String sayHello(){
+
+	@RequestMapping(value = "/greeting", method = { RequestMethod.GET })
+	public @ResponseBody String sayHello() {
 		return "hello user!!!";
+	}
+
+	@RequestMapping(value = "/greetUser", method = { RequestMethod.GET })
+	public @ResponseBody String sayHelloCustomized(@RequestParam(defaultValue = "") String name) {
+		name = name == null || name.trim().length() == 0 ? "Guest" : name.trim();
+		return "hello " + name + "!!!";
 	}
 }
